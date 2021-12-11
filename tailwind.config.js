5ee702cf1fb343e750ce96335b5e7995e8440984
Sean <seandp1a@gmail.com>
2021-12-11 16:35:30 +0800

    home/test/navbar component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode:'jit',
+  purge:{
+    enabled: true,
+    content: ["./src/**/*.html", "./src/**/*.scss"],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// npx tailwindcss init --full 可取得完整預設配置檔
+// 詳見 https://unpkg.com/browse/tailwindcss@2.2.16/stubs/defaultConfig.stub.js
