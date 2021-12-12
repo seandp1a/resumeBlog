9de82223763bf436e1502c3d090255834be0032d
Sean <seandp1a@gmail.com>
2021-12-13 00:09:48 +0800

    little animation bounce & fade in

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
   },
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.9s ease-out'
+      }
+    },
   },
   plugins: [],
 }
