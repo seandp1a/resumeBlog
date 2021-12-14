edac0f69fa68d694d7df85dd127642cf937efd76
Sean <seandp1a@gmail.com>
2021-12-14 17:05:55 +0800

    configService 可存取 config.json 資料

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,12 @@ import { HomeComponent } from './page/home/home.component';
 import { TestComponent } from './page/test/test.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ConfigService } from './core/config.service';
+import { HttpClientModule } from '@angular/common/http';
+
+export const configFactory = (config: ConfigService) => {
+  return () => config.loadConfig();
+}
 
 @NgModule({
   declarations: [
@@ -18,9 +24,17 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configFactory,
+      deps: [ConfigService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
