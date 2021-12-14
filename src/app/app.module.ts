3e2f1017ab79120742dec61681c1b0e6de32cbcb
Sean <seandp1a@gmail.com>
2021-12-14 17:46:43 +0800

    add @nguniversal/express-engine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
