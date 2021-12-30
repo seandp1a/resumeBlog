e29d8f439acf41bb51223a95d6de937b70b0db37
Sean <seandp1a@gmail.com>
2021-12-30 14:42:35 +0800

    socketIO

--- src/app/core/socket.service.ts
+++ src/app/core/socket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { io } from 'socket.io-client'
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +10,56 @@ export class SocketService {
 
   ws!: WebSocket;
   constructor() { }
+  public url = 'ws://localhost:8001'
+  public channel :any;
+  private socket: any;
+
+
+  sendMessage(message: any) {
+    this.socket.emit('aaa', message);
+  }
+
+  setNickName(name:string){
+    this.socket.emit('setnickname',name);
+  }
+  setChannel(channel:any){
+    this.socket.emit('join',channel);
+  }
+
+  getMessages(): Observable<any> {
+    return new Observable(observer => {
+      this.socket = io(this.url, { withCredentials: true });
+      this.socket.on('nicknamesuccess', (data:any) => {
+        observer.next(data);
+      });
+      this.socket.on('nicknamefail', (data:any) => {
+        observer.next(data);
+      });
+      this.socket.on('joinroomsuccess', (data:any) => {
+        observer.next(data);
+      });
+      return () => {
+        this.socket.disconnect();
+      }
+    })
+  }
+
   createObservableSocket(url: string): Observable<any> {
 
     this.ws = new WebSocket(url)
     return new Observable(
       observer => {
-        this.ws.onopen=(event)=>observer.next(event)
+        this.ws.onopen = (event) => observer.next(event)
         this.ws.onmessage = (event) => observer.next(event)
         this.ws.onerror = (event) => observer.error(event)
-        this.ws.onclose = (event) => {observer.next(event); observer.complete();}
+        this.ws.onclose = (event) => { observer.next(event); observer.complete(); }
       })
 
   }
-  test(){
+  test() {
     const wss = webSocket('wss://localhost:8000');
-    wss.subscribe(res=>{
-      console.log('message',res)
+    wss.subscribe(res => {
+      console.log('message', res)
     });
     wss.next('content')
   }
--- src/app/page/test/test.component.ts
+++ src/app/page/test/test.component.ts
@@ -9,51 +9,95 @@ import { map } from 'rxjs';
 })
 export class TestComponent implements OnInit {
 
-  msg:string[] = [];
-  inputMsg:string = '';
-  socketUrl:string = 'wss://echo.websocket.events/';
+  msg: string[] = [];
+  inputMsg: string = '';
+  socketUrl: string = 'wss://echo.websocket.events/';
+
+  inputIoMsg: string = '';
+  socketIoUrl: string = 'ws://localhost:8001';
+  nickName: string = '';
+  channel:  any= 0;
+
+  connection: any;
+  ioMsg: any[] = [];
+
   isConnected = false;
+  isIoConnected = false;
 
-  constructor(public socketSvc:SocketService) { }
+  roomMap = ['room1','room2','room3']
 
-  public connect(){
-    if(this.socketSvc.ws&&this.socketSvc.ws.readyState===WebSocket.OPEN){
+  constructor(public socketSvc: SocketService) { }
+
+  public connect() {
+    if (this.socketSvc.ws && this.socketSvc.ws.readyState === WebSocket.OPEN) {
       return;
     }
     this.socketSvc.createObservableSocket(this.socketUrl)
-    // .pipe(map(event => JSON.parse(event)))
-    .subscribe((event)=>{
-      switch(event.type){
-        case 'open' :{
-          this.msg.push('--- connected ---');
-          this.isConnected = true;
-          break;
-        }
-        case 'message' :{
-          this.msg.push(`msg：${event.data} `) ;
-          break;
-        }
-        case 'close':{
-          this.msg.push('--- disconnected ---');
-          this.isConnected = false;
-          break;
-        }
+      // .pipe(map(event => JSON.parse(event)))
+      .subscribe((event) => {
+        switch (event.type) {
+          case 'open': {
+            this.msg.push('--- connected ---');
+            this.isConnected = true;
+            break;
+          }
+          case 'message': {
+            this.msg.push(`msg：${event.data} `);
+            break;
+          }
+          case 'close': {
+            this.msg.push('--- disconnected ---');
+            this.isConnected = false;
+            break;
+          }
 
-      }
-      // this.msg = event;
-      console.log(event)
-    });
+        }
+        // this.msg = event;
+        console.log(event)
+      });
   }
-  public disConnect(){
-    if(this.socketSvc.ws&&this.socketSvc.ws.readyState===WebSocket.OPEN){
+  public disConnect() {
+    if (this.socketSvc.ws && this.socketSvc.ws.readyState === WebSocket.OPEN) {
       this.socketSvc.ws.close();
     }
   }
-  public send(){
-    if(this.socketSvc.ws&&this.socketSvc.ws.readyState===WebSocket.OPEN){
+  public send() {
+    if (this.socketSvc.ws && this.socketSvc.ws.readyState === WebSocket.OPEN) {
       this.socketSvc.ws.send(this.inputMsg);
       this.inputMsg = '';
     }
+  }
+
+  public sendIo() {
+    this.socketSvc.sendMessage(this.inputIoMsg);
+  }
+  public connectIo() {
+    this.socketSvc.url = this.socketIoUrl;
+    this.socketSvc.channel = this.channel;
+    this.connection = this.socketSvc.getMessages()
+      .subscribe(message => {  // 組件初始化時訂閱信息
+        this.isIoConnected = true;
+        this.socketSvc.setNickName(this.nickName);
+        this.socketSvc.setChannel(this.roomMap[this.channel]);
+        this.ioMsg.push(message);
+        console.log(message);
+      });
+  }
+
+  public setNickName(){
+    this.socketSvc.setNickName(this.nickName);
+
+  }
+  public disconnectIo() {
+    this.connection.unsubscribe();
+    this.isIoConnected = false;
+  }
+
+  public disableConnect() {
+    const noNickName = this.nickName === '';
+    const noChannel = this.channel === 0;
+    const noUrl = this.socketIoUrl ==='';
+    return (noChannel||noUrl||noNickName);
   }
 
   ngOnInit(): void {
